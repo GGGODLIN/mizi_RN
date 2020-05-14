a1553dc2fc331c51e48e17f08b6af676fab7477d
GGGODLIN <qwe70301@gmail.com>
2020-05-14 12:13:20 +0800

    Update BottomTab.js

--- src/BottomTab.js
+++ src/BottomTab.js
@@ -29,7 +29,7 @@ import {
   Button,
 } from 'react-native-paper';
 const args = {
-  number: '089357328', // String value with the number to call
+  number: '089960160', // String value with the number to call
   prompt: false, // Optional boolean property. Determines if the user should be prompt prior to the call
 };
 const Tab = createMaterialBottomTabNavigator();
