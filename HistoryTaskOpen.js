aaf8a33c7601efea145905a8e23ac6d1b2af636c
GGGODLIN <qwe70301@gmail.com>
2020-05-14 16:35:20 +0800

    未來陪同

--- HistoryTaskOpen.js
+++ HistoryTaskOpen.js
@@ -241,6 +241,7 @@ const HistoryTaskOpen = props => {
   if (isLoading) {
     setLoading(false);
     console.log('info screen is loading...');
+    let withPeople = caseStatus[detailIndex]<6?taskData[detailIndex].OrderDetails.FamilyWith:taskData[detailIndex].OrderDetails.RealFamilyWith;
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <ActivityIndicator animating={true} size='large' />
@@ -284,7 +285,7 @@ const HistoryTaskOpen = props => {
             </View>
             <View style={styles.titleRight}>
               <Text style={{color: 'white', fontSize: 20}}>
-                {'個案' + 1 + '/' + '陪同' + (taskData[detailIndex].OrderDetails.RealFamilyWith + taskData[detailIndex].OrderDetails.RealForeignFamilyWith)}
+                {'個案' + 1 + '/' + '陪同' + withPeople}
               </Text>
             </View>
           </View>
