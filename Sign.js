248058c7fd0d58541f917d8640ea6d04c9aba38e
GGGODLIN <qwe70301@gmail.com>
2020-05-26 09:51:36 +0800

    signbug

--- Sign.js
+++ Sign.js
@@ -8,21 +8,24 @@ var ReactNative = require('react-native');
 
 var {Component} = React;
 
-var {AppRegistry, StyleSheet, Text, View, TouchableHighlight} = ReactNative;
+var {AppRegistry, StyleSheet, Text, View, TouchableHighlight,Alert} = ReactNative;
 
 import SignatureCapture from 'react-native-signature-capture';
 
 class RNSignatureExample extends Component {
     constructor(props) {
     super(props);
+    this.state = {isDragged: false};
     console.log(this.props.handleSavePic);
     this._onSaveEvent = this._onSaveEvent.bind(this);
+    this._onDragEvent = this._onDragEvent.bind(this);
   }
 
   _onSaveEvent(result) {
     //result.encoded - for the base64 encoded png
     //result.pathName - for the file path name
     console.log(result);
+    this.setState({isDragged: false});
     this.props.handleSavePic(result);
   }
   render() {
@@ -66,15 +69,28 @@ class RNSignatureExample extends Component {
   }
 
   saveSign() {
-    this.refs['sign'].saveImage();
+    if (this.state.isDragged){
+      this.refs['sign'].saveImage();
+    }
+    else{
+      Alert.alert('請在簽完名後再按完成', ' ', [
+              {
+                text: '確定',
+                onPress: () => {},
+              },
+            ]);
+    }
   }
 
   resetSign() {
     this.refs['sign'].resetImage();
+    this.setState({isDragged: false});
+    console.log('resetSign');
   }
 
   _onDragEvent() {
     // This callback will be called when the user enters signature
+    this.setState({isDragged: true});
     console.log('dragged');
   }
 }
